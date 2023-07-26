aa113bf7c792e8bcf19964635e1fcda7d3f24ab7
Vishnubhotla <sunilvbs@3c0630190b8c.ant.amazon.com>
2023-07-26 18:03:19 -0400

    Updated 07-26-2023

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from redshift_connection import RedshiftConnection
-st.title('Amazon Redshift Connector Test')
+st.title('Amazon Redshift Connector Test - by Sunil Vishnubhotla')
 
 conn = st.experimental_connection('pets_db', type=RedshiftConnection)
 pet_owners = conn.query('SELECT * FROM dev.public.users limit 5')
